47582a1598dc847c6d4041a2f6ce9114aa841798
IT21258626 <it21258626@my.sliit.lk>
2022-02-22 21:50:31 +0530

    Tute 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1,mark2,total;
+  float avg;
   
+  printf("Enter mark1 : ");
+  scanf("%d", &mark1);
+
+  printf("Enter mark2 : ");
+  scanf("%d", &mark2);
+
+  total = mark1 + mark2;
+  avg = total/2;
+
+  printf("Average is : %.2f", avg);
+
   return 0;
 }
 
