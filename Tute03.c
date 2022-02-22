1fa29a2350e76fa740d36df763696df758cff6d6
IT21258626 <it21258626@my.sliit.lk>
2022-02-22 22:02:02 +0530

    Tute 3

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,19 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
-  
-  return 0;
-}
-
+#include <stdio.h>  
+#include <conio.h>  
+void main()  
+{  
+    int num, i, sum = 0;  //declare variables  
+    printf(" Enter a positive number: ");  
+    scanf("%d", &num);   //get user inputs
+      
+    for (i = 0; i <= num; i++)  
+    {  
+        sum = sum + i;   //calculation
+    }  
+      
+    printf("\n Sum of the first %d number is: %d", num, sum);   //display output
+    getch();  
+}  
